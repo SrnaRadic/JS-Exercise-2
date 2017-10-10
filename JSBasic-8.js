b42c2968c066e3609fb7b6ca0bfd53dd3f38729d
Srna Radic <srna6789@hotmail.com>
2017-10-10 13:32:26 +0200

    Fixing js + fixing media query for mobile devices

--- JSBasic-8.js
+++ JSBasic-8.js
@@ -1,7 +1,7 @@
 function MultiplyFunction() {
-  var x = document.getElementById('1stNo').value;
-  var y = document.getElementById('2ndNo').value;
-  if (document.getElementById(('1stNo' && '2ndNo')).value == "") {
+  var x = parseFloat(document.getElementById('1stNo').value);
+  var y = parseFloat(document.getElementById('2ndNo').value);
+  if (isNaN(x) || isNaN(y)) {
     document.getElementById('result').innerHTML = "You left some or all the fields empty"
   } else {
     document.getElementById('result').innerHTML = x*y;
@@ -9,34 +9,31 @@ function MultiplyFunction() {
 }
 
 function DivideFunction() {
-  var x = document.getElementById('1stNo').value;
-  var y = document.getElementById('2ndNo').value;
-  if (document.getElementById(('1stNo' && '2ndNo')).value == "") {
+  var x = parseFloat(document.getElementById('1stNo').value);
+  var y = parseFloat(document.getElementById('2ndNo').value);
+  if (isNaN(x) || isNaN(y)) {
     document.getElementById('result').innerHTML = "You left some or all the fields empty"
   } else {
-      document.getElementById('result').innerHTML = x/y;
+    document.getElementById('result').innerHTML = x/y;
   }
 }
 
 function SumFunction() {
-  var x = document.getElementById('1stNo').value;
-  var y = document.getElementById('2ndNo').value;
-  var x1 = parseInt(x);
-  var y1 = parseInt(y);
-  if (document.getElementById(('1stNo' && '2ndNo')).value == "") {
+  var x = parseFloat(document.getElementById('1stNo').value);
+  var y = parseFloat(document.getElementById('2ndNo').value);
+  if (isNaN(x) || isNaN(y)) {
     document.getElementById('result').innerHTML = "You left some or all the fields empty"
   } else {
-      document.getElementById('result').innerHTML = x1+y1;
+    document.getElementById('result').innerHTML = x+y;
   }
 }
 
 function SubtractFunction() {
-  var x = document.getElementById('1stNo').value;
-  var y = document.getElementById('2ndNo').value;
-
-    if (document.getElementById(('1stNo' && '2ndNo')).value == "") {
-      document.getElementById('result').innerHTML = "You left some or all the fields empty"
-    } else {
-      document.getElementById('result').innerHTML = x-y;
-    }
+  var x = parseFloat(document.getElementById('1stNo').value);
+  var y = parseFloat(document.getElementById('2ndNo').value);
+  if (isNaN(x) || isNaN(y)) {
+    document.getElementById('result').innerHTML = "You left some or all the fields empty"
+  } else {
+    document.getElementById('result').innerHTML = x-y;
+  }
 }
